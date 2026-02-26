781d1b348dee2a7de82f606338e3b7c8f3b9b1af
sigarachi <lalalabtv@gmail.com>
2026-02-26 20:19:40 +0300

    movable points

--- client/src/features/map/components/drawer/index.tsx
+++ client/src/features/map/components/drawer/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   Circle,
   CircleMarker,
@@ -10,10 +10,18 @@ import { useDraw } from "@shared/store/draw";
 
 export const Drawer = () => {
   const [positions, setPositions] = useState<Array<Array<number>>>([]);
+  const [selected, setSelected] = useState<number | null>(null);
+  const [dragging, setDragging] = useState<{
+    active: boolean;
+    index: number | null;
+  }>({
+    active: false,
+    index: null,
+  });
 
   const { updatePolygons, currentMarker } = useDraw();
 
-  useMapEvents({
+  const map = useMapEvents({
     //@ts-ignore
     click: (e) => {
       if (currentMarker) {
@@ -28,7 +36,54 @@ export const Drawer = () => {
         setPositions(newPositions);
       }
     },
+    //@ts-ignore
+    mousemove: (e) => {
+      if (dragging.active && dragging.index !== null) {
+        const newPositions = [...positions];
+        newPositions[dragging.index] = [e.latlng.lat, e.latlng.lng];
+        updatePolygons(newPositions);
+        setPositions(newPositions);
+
+        map.dragging.disable();
+      }
+    },
+    mouseup: () => {
+      if (dragging.active) {
+        setDragging({ active: false, index: null });
+
+        map.dragging.enable();
+      }
+    },
   });
+
+  const handleMouseDown = useCallback((index: number, e: unknown) => {
+    //@ts-ignore
+    e.originalEvent.stopPropagation();
+    //@ts-ignore
+    e.originalEvent.preventDefault();
+
+    setDragging({
+      active: true,
+      index: index,
+    });
+
+    map.dragging.disable();
+  }, []);
+
+  const handleDelete = useCallback(
+    (event: KeyboardEvent) => {
+      const key = event.key;
+
+      if (key === "Delete" && selected) {
+        const newPositions = [...positions];
+        newPositions.splice(selected, 1);
+        updatePolygons(newPositions);
+        setPositions(newPositions);
+        setSelected(null);
+      }
+    },
+    [positions, selected, updatePolygons],
+  );
 
   useEffect(() => {
     if (currentMarker) {
@@ -38,6 +93,14 @@ export const Drawer = () => {
     }
   }, [currentMarker]);
 
+  useEffect(() => {
+    window.addEventListener("keydown", handleDelete);
+
+    return () => {
+      window.removeEventListener("keydown", handleDelete);
+    };
+  }, [handleDelete]);
+
   return (
     <>
       {currentMarker && (
@@ -46,7 +109,16 @@ export const Drawer = () => {
             <>
               <Polygon positions={positions} />
               {positions.map((pos, index) => (
-                <Circle key={index} center={pos} radius={200}>
+                <Circle
+                  key={index}
+                  center={pos}
+                  radius={5000}
+                  eventHandlers={{
+                    //@ts-ignore
+                    mousedown: (e) => handleMouseDown(index, e),
+                    click: () => setSelected(index),
+                  }}
+                >
                   <Popup>
                     Вершина {index + 1}
                     <br />

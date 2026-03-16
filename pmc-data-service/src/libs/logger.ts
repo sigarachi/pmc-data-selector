58a6dd4c9425423c9deff1899a6548edb1e664f3
sigarachi <lalalabtv@gmail.com>
2026-03-16 21:04:12 +0300

    logging to elastic

--- pmc-data-service/src/libs/logger.ts
+++ pmc-data-service/src/libs/logger.ts
@@ -1,15 +1,9 @@
 import winston from "winston";
-import Transport from "winston-transport";
 import { Client } from "@opensearch-project/opensearch";
 import { OpensearchTransport } from "winston-opensearch";
 
-const { combine, timestamp, errors, json, printf } = winston.format;
-
-/**
- * OpenSearch client
- */
-const opensearch = new Client({
-  node: process.env.OPENSEARCH_URL || "http://localhost:9200",
+const opensearchClient = new Client({
+  node: process.env.OPENSEARCH_URL || "https://localhost:9200",
   auth: {
     username: process.env.OPENSEARCH_USERNAME || "admin",
     password: process.env.OPENSEARCH_PASSWORD || "admin",
@@ -19,25 +13,9 @@ const opensearch = new Client({
   },
 });
 
-/**
- * Custom Winston transport for OpenSearch
- *
- */
-
-const opensearchClient = new Client({
-  node: process.env.OPENSEARCH_URL || "https://localhost:9200",
-  auth: {
-    username: process.env.OPENSEARCH_USERNAME || "admin",
-    password: process.env.OPENSEARCH_PASSWORD || "admin",
-  },
-  ssl: {
-    rejectUnauthorized: false, // Secure in production
-  },
-});
-
 const esTransportOpts = {
-  level: "info", // Minimum log level to send to OpenSearch
-  client: opensearchClient, // Use the pre-configured client
+  level: "info",
+  client: opensearchClient,
   indexPrefix: "pmc-service",
   transformer: (logData: winston.LogEntry) => {
     const { level, message, meta } = logData;
@@ -46,38 +24,30 @@ const esTransportOpts = {
       severity: level,
       message: message,
       fields: {
-        ...meta, // Spread all metadata into the 'fields' object
+        ...meta,
         environment: process.env.NODE_ENV || "development",
       },
     };
   },
-  // Optional: Handle bulk write failures gracefully
+
   retryLimit: 5,
 };
 
-const consoleFormat = printf(({ level, message, timestamp, stack }) => {
-  return `${timestamp} [${level}] ${stack || message}`;
-});
-
-/**
- * Winston Logger
- */
 const logger = winston.createLogger({
-  level: "info", // Minimum level to process
+  level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json(), // Structured JSON logs are best for OpenSearch
+    winston.format.json(),
   ),
-  defaultMeta: { service: "user-service" }, // Metadata added to all logs
+  defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.Console({
-      // Always log to console for development
       format: winston.format.combine(
         winston.format.colorize(),
         winston.format.simple(),
       ),
     }),
-    new OpensearchTransport(esTransportOpts as any), // Add the OpenSearch transport
+    new OpensearchTransport(esTransportOpts as any),
   ],
 });
 
